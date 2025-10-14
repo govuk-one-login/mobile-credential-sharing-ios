20002faf61fd99fc3f4d3ca83f8f571aae40bfa6
Jack Winterschladen <jwinterschladen@deloitte.co.uk>
2025-10-14 16:18:40 +0100

    Add initial file set-up & view

---
+++ mobile-credential-sharing-ios/mobile-credential-sharing-iosTests/mobile_credential_sharing_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  mobile_credential_sharing_iosTests.swift
+//  mobile-credential-sharing-iosTests
+//
+//  Created by Jack Winterschladen on 14/10/2025.
+//
+
+import Testing
+@testable import mobile_credential_sharing_ios
+
+struct mobile_credential_sharing_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ mobile-credential-sharing-ios/mobile-credential-sharing-iosUITests/mobile_credential_sharing_iosUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  mobile_credential_sharing_iosUITestsLaunchTests.swift
+//  mobile-credential-sharing-iosUITests
+//
+//  Created by Jack Winterschladen on 14/10/2025.
+//
+
+import XCTest
+
+final class mobile_credential_sharing_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
