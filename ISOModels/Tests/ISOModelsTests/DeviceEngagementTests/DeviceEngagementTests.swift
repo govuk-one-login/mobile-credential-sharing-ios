4ec042893fd07b0b17f4387749f440188ffcb331
Jack Winterschladen <jack.winterschladen@digital.cabinet-office.gov.uk>
2025-10-23 14:52:23 +0100

    Add DeviceEngagement CBOR tests

--- ISOModels/Tests/ISOModelsTests/DeviceEngagementTests/DeviceEngagementTests.swift
+++ ISOModels/Tests/ISOModelsTests/DeviceEngagementTests/DeviceEngagementTests.swift
@@ -1,6 +1,7 @@
 import Foundation
 @testable import ISOModels
 import Testing
+import SwiftCBOR
 
 struct DeviceEngagementTests {
     
@@ -27,5 +28,57 @@ struct DeviceEngagementTests {
         )
         
         #expect(sut.version == "1.0")
+    }
+    
+    @Test("Correctly encodes to CBOR with no device retrieval methods")
+    func encodesToCBORNoRetrievalMethods() throws {
+        let sut = DeviceEngagement(
+            security: Security(
+                cipherSuiteIdentifier: CipherSuite(identifier: 1),
+                eDeviceKey: key,
+            ),
+            deviceRetrievalMethods: []
+        )
+        
+        #expect(
+            sut.toCBOR(options: CBOROptions()) == [
+                0: "1.0",
+                1: [
+                    1,
+                    .tagged(
+                        .encodedCBORDataItem,
+                        .byteString(key.encode(options: CBOROptions()))
+                    )
+                ]
+            ]
+        )
+    }
+    
+    @Test("Correctly encodes to CBOR with device retrieval methods")
+    func encodesToCBORWithRetrievalMethods() throws {
+        let method: DeviceRetrievalMethod = .bluetooth(
+            .peripheralOnly(
+                PeripheralMode(
+                    uuid: UUID.init(),
+                    address: "test"
+                )
+            )
+        )
+        let sut = DeviceEngagement(
+            security: Security(
+                cipherSuiteIdentifier: CipherSuite(identifier: 1),
+                eDeviceKey: key,
+            ),
+            deviceRetrievalMethods: [method]
+        )
+        
+        let encodedDeviceEngagement = sut.toCBOR(options: CBOROptions())
+        let encodedRetrievalMethod = method.toCBOR(options: CBOROptions())
+        
+        #expect(encodedDeviceEngagement == [
+            0: "1.0",
+            1: [1, .tagged(.encodedCBORDataItem, .byteString(key.encode(options: CBOROptions())))],
+            2: [encodedRetrievalMethod]
+        ])
     }
 }
