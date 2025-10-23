61e37a1e8a82a7512d1267573bf3ff3674b3c418
Jack Winterschladen <jack.winterschladen@digital.cabinet-office.gov.uk>
2025-10-23 13:10:18 +0100

    Remove duplicate & update tests

--- ISOModels/Tests/ISOModelsTests/DeviceRetrievalMethodTests.swift
+++ ISOModels/Tests/ISOModelsTests/DeviceRetrievalMethodTests.swift
@@ -1,15 +1,28 @@
 @testable import ISOModels
+import Foundation
 import Testing
 
 @Suite("DeviceRetrievalMethod tests")
 struct DeviceRetrievalMethodTests {
     @Test("Type values are as defined in the ISO specification")
     func typeValueIsCorrect() async throws {
-        #expect(DeviceRetrievalMethod.bluetooth.type == 2)
+        #expect(
+            DeviceRetrievalMethod
+                .bluetooth(
+                    .peripheralOnly(
+                        PeripheralMode(uuid: UUID(), address: "test")
+                    )
+                ).type == 2
+        )
     }
 
     @Test("Version values are as defined in the ISO specification")
     func versionValueIsCorrect() async throws {
-        #expect(DeviceRetrievalMethod.bluetooth.version == 1)
+        #expect(DeviceRetrievalMethod
+            .bluetooth(
+                .peripheralOnly(
+                    PeripheralMode(uuid: UUID(), address: "test")
+                )
+            ).version == 1)
     }
 }
