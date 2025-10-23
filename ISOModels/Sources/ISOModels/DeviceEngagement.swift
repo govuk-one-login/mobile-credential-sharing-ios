59f19bb4da4ee73f9a1f8c89d99addabb896319f
Jack Winterschladen <jack.winterschladen@digital.cabinet-office.gov.uk>
2025-10-23 11:50:54 +0100

    Rename test folder & add DE init

--- ISOModels/Sources/ISOModels/DeviceEngagement.swift
+++ ISOModels/Sources/ISOModels/DeviceEngagement.swift
@@ -1,7 +1,17 @@
 import Foundation
 
 struct DeviceEngagement {
-    let version: String = "1.0"
+    let version: String
     let security: Security
     let deviceRetrievalMethods: [DeviceRetrievalMethod]?
+    
+    init(
+        version: String = "1.0",
+        security: Security,
+        deviceRetrievalMethods: [DeviceRetrievalMethod]?
+    ) {
+        self.version = version
+        self.security = security
+        self.deviceRetrievalMethods = deviceRetrievalMethods
+    }
 }
