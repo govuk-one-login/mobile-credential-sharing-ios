c9b7717f4cf59676e9549041b5186747e128d65d
Jack Winterschladen <jack.winterschladen@digital.cabinet-office.gov.uk>
2025-10-23 17:06:45 +0100

    Temporary disable swiftlint line-length

--- mobile-credential-sharing-ios/mobile-credential-sharing-ios/QRCodeViewController.swift
+++ mobile-credential-sharing-ios/mobile-credential-sharing-ios/QRCodeViewController.swift
@@ -16,7 +16,8 @@ class QRCodeViewController: UIViewController {
         )]
         
         view.backgroundColor = .systemBackground
-                
+        
+        // swiftlint:disable:next line_length
         print("the base64 encoded CBOR is: ", Data(DeviceEngagement(security: Security(cipherSuiteIdentifier: CipherSuite.iso18013, eDeviceKey: EDeviceKey(curve: .p256, xCoordinate: [], yCoordinate: [])), deviceRetrievalMethods: [.bluetooth(.peripheralOnly(PeripheralMode(uuid: UUID(), address: "mock-address")))]).toCBOR().encode()).base64EncodedString())
         
         do {
