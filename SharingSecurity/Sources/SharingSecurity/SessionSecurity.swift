3ca79f987225b9b59914573580389a320caa0ffe
jwinterschladen-dd <jack.winterschladen@digital.cabinet-office.gov.uk>
2025-10-28 11:00:47 +0000

    Add session decryption class

--- SharingSecurity/Sources/SharingSecurity/SessionSecurity.swift
+++ SharingSecurity/Sources/SharingSecurity/SessionSecurity.swift
@@ -16,3 +16,53 @@ protocol EncryptionParameters {
     var sharedInfo: Data { get }
     var identifier: Data { get }
 }
+
+final class SessionDecryption: SessionSecurity {
+    private let privateKey: P256.KeyAgreement.PrivateKey
+    
+    var publicKey: P256.KeyAgreement.PublicKey {
+        privateKey.publicKey
+    }
+    
+    init(privateKey: P256.KeyAgreement.PrivateKey) {
+        self.privateKey = privateKey
+    }
+    
+    func decryptData(
+        _ data: [UInt8],
+        salt: [UInt8],
+        encryptedWith theirPublicKey: P256.KeyAgreement.PublicKey,
+        by parameters: any EncryptionParameters
+    ) throws -> Data {
+        let secret = try privateKey
+            .sharedSecretFromKeyAgreement(with: theirPublicKey)
+        
+        let symmetricKey = secret.hkdfDerivedSymmetricKey(
+            using: SHA256.self,
+            salt: salt,
+            sharedInfo: parameters.sharedInfo,
+            outputByteCount: 32
+        )
+        
+        let nonce = try makeNonce(identifier: parameters.identifier)
+        let box = try AES.GCM.SealedBox(combined: Data(nonce) + data)        
+        return try AES.GCM.open(box, using: symmetricKey)
+    }
+    
+    private func makeNonce(
+        _ counter: UInt32 = 1,
+        identifier: Data
+    ) throws -> AES.GCM.Nonce {
+        var dataNonce = Data()
+        dataNonce.append(identifier)
+        dataNonce.append(Data(counter.bigEndianByteArray))
+        let nonce = try AES.GCM.Nonce(data: dataNonce)
+        return nonce
+    }
+}
+
+extension UInt32 {
+    var bigEndianByteArray: [UInt8] {
+        withUnsafeBytes(of: bigEndian, Array.init)
+    }
+}
