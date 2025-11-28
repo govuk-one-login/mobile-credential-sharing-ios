d08be3a5efc3c71bdf800e325a1f5b729ca7866d
jwinterschladen-dd <jack.winterschladen@digital.cabinet-office.gov.uk>
2025-11-28 12:09:29 +0000

    Linting comment space

--- Bluetooth/Tests/BluetoothTests/ConnectionStateTests.swift
+++ Bluetooth/Tests/BluetoothTests/ConnectionStateTests.swift
@@ -1,10 +1,10 @@
 // TODO: DCMAW-17058 - To implement with receiving SessionEstablishment
-//@testable import Bluetooth
-//import Foundation
-//import Testing
+// @testable import Bluetooth
+// import Foundation
+// import Testing
 //
-//@Suite("Connection State Tests")
-//struct ConnectionStateTests {
+// @Suite("Connection State Tests")
+// struct ConnectionStateTests {
 //    @Test("Start state has correct data value")
 //    func startDataValue() {
 //        #expect(ConnectionState.start.data == Data([0x01]))
@@ -14,4 +14,4 @@
 //    func endDataValue() {
 //        #expect(ConnectionState.end.data == Data([0x02]))
 //    }
-//}
+// }
