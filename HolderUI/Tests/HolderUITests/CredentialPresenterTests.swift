14615a4d75961f15c23ad48a59a94d189d648bac
RD <richard.das@digital.cabinet-office.gov.uk>
2025-12-15 14:02:03 +0000

    Fixed let formating to resolve warnings.

--- HolderUI/Tests/HolderUITests/CredentialPresenterTests.swift
+++ HolderUI/Tests/HolderUITests/CredentialPresenterTests.swift
@@ -1,7 +1,8 @@
-@testable import HolderUI
 import ISOModels
 import Testing
 import UIKit
+
+@testable import HolderUI
 
 @MainActor
 @Suite("CredentialPresenter Tests")
@@ -10,37 +11,35 @@ struct CredentialPresenterTests {
     init() {
         sut = CredentialPresenter()
     }
-    
+
     @Test("Present credential func initialises QRCodeViewController")
     func presentCredentialInitialisesQRVC() {
         let vc = EmptyViewController()
-        let _ = UINavigationController(
-            rootViewController: vc
-        )
+        _ = UINavigationController(rootViewController: vc)
+
         #expect(sut.qrCodeViewController == nil)
         sut.presentCredential(Data(), over: vc)
-        
+
         #expect(sut.qrCodeViewController != nil)
         #expect(sut.qrCodeViewController?.delegate === sut)
     }
-    
+
     @Test("Did tap navigate to settings func re-inits PeripheralSession")
     func didTapNavigateToSettingsReInitsPeripheralSession() throws {
         let initialPeripheralSession = sut.peripheralSession
         sut.didTapNavigateToSettings()
-        
+
         #expect(sut.peripheralSession !== initialPeripheralSession)
         #expect(sut.peripheralSession.delegate === sut)
     }
-    
+
     @Test(
         "peripheralSessionDidUpdateState func passes showQRCode when no error"
     )
     func passesShowQRCodeWhenNoError() throws {
         let vc = EmptyViewController()
-        let _ = UINavigationController(
-            rootViewController: vc
-        )
+        _ = UINavigationController(rootViewController: vc)
+
         sut.presentCredential(Data(), over: vc)
         sut.peripheralSessionDidUpdateState(withError: nil)
         let qrCodeViewController = try #require(sut.qrCodeViewController)
@@ -49,40 +48,38 @@ struct CredentialPresenterTests {
                 .contains(where: { $0 == qrCodeViewController.qrCodeImageView })
         )
     }
-    
+
     @Test("peripheralSessionDidUpdateState func passes showSettingsButton when given state error")
     func passesShowSettingsButtonWhenPassedError() throws {
         let vc = EmptyViewController()
-        let _ = UINavigationController(
-            rootViewController: vc
-        )
+        _ = UINavigationController(rootViewController: vc)
+
         sut.presentCredential(Data(), over: vc)
         sut.peripheralSessionDidUpdateState(withError: .notPoweredOn(.poweredOff))
         let qrCodeViewController = try #require(sut.qrCodeViewController)
-        #expect(qrCodeViewController.view.subviews.contains(where: {
-            $0 is UIButton &&
-            ($0 as? UIButton)?.titleLabel?.text == "Go to settings"
-        }))
+        #expect(
+            qrCodeViewController.view.subviews.contains(where: {
+                $0 is UIButton && ($0 as? UIButton)?.titleLabel?.text == "Go to settings"
+            })
+        )
     }
-    
+
     @Test(
         "peripheralSessionDidUpdateState func navigates to error view when given permissions error"
     )
     func navigatesToErrorViewWhenPassedError() throws {
         let vc = EmptyViewController()
-        _ = UINavigationController(
-            rootViewController: vc
-        )
+        _ = UINavigationController(rootViewController: vc)
+
         sut.presentCredential(Data(), over: vc)
         sut.peripheralSessionDidUpdateState(withError: .permissionsNotGranted(.denied))
-        
+
         let navigationController = try #require(sut.navigationController)
-        
+
         #expect(
             navigationController.viewControllers
                 .contains(where: { (type(of: $0) == ErrorViewController.self) })
         )
-        
     }
 }
 
