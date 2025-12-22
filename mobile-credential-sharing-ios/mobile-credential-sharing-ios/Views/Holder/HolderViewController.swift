0fd242fe425a7b3fc6e563f80b72896a9c044be6
Richard Das <richard.das@digital.cabinet-office.gov.uk>
2025-12-22 12:40:10 +0000

    Start work on Holder/Verifier UI separation

--- mobile-credential-sharing-ios/mobile-credential-sharing-ios/Views/Holder/HolderViewController.swift
+++ mobile-credential-sharing-ios/mobile-credential-sharing-ios/Views/Holder/HolderViewController.swift
@@ -1,95 +1,47 @@
-import HolderUI
 import UIKit
+import HolderUI
 
 class HolderViewController: UIViewController {
     let activityIndicator = UIActivityIndicatorView(style: .large)
-    let navigateButton = UIButton(type: .system)
     var credentialPresenter: CredentialPresenter?
     
+    static let presentButtonIdentifier = "PresentCredentialButton"
+    static let activityIndicatorIdentifier = "CredentialActivityIndicator"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .systemBackground
-        
-        setupTitle()
-        setupNavigateButton()
-        setupActivityIndicator()
+        restorationIdentifier = "HolderViewController"
+        title = "Holder"
+        setupView()
     }
     
-    private func setupTitle() {
-        let titleLabel: UILabel = {
-            let label = UILabel()
-            label.text = "Welcome to GOV.UK Wallet Sharing"
-            label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
-            label.textAlignment = .center
-            label.numberOfLines = 0
-            label.translatesAutoresizingMaskIntoConstraints = false
-            return label
-        }()
+    private func setupView() {
+        let presentButton = UIButton(type: .system)
+        presentButton.setTitle("Present Credential", for: .normal)
+        presentButton.addTarget(self, action: #selector(presentButtonTapped), for: .touchUpInside)
+        presentButton.translatesAutoresizingMaskIntoConstraints = false
+        presentButton.accessibilityIdentifier = HolderViewController.presentButtonIdentifier
+        view.addSubview(presentButton)
         
-        view.addSubview(titleLabel)
-
-        NSLayoutConstraint.activate(
-            [
-                titleLabel.centerXAnchor
-                    .constraint(equalTo: view.centerXAnchor),
-                titleLabel.centerYAnchor
-                    .constraint(
-                        equalTo: view.safeAreaLayoutGuide.topAnchor,
-                    ),
-                titleLabel.leadingAnchor
-                    .constraint(equalTo: view.leadingAnchor, constant: 12),
-                titleLabel.trailingAnchor
-                    .constraint(equalTo: view.trailingAnchor, constant: -12)
-            ]
-        )
-    }
-    
-    private func setupNavigateButton() {
-        navigateButton.configuration = .bordered()
-        navigateButton.configuration?.baseBackgroundColor = .systemGreen
-        navigateButton.configuration?.baseForegroundColor = .white
-        navigateButton.configuration?.contentInsets = .init(
-            top: 16,
-            leading: 16,
-            bottom: 16,
-            trailing: 16
-        )
-            
-        navigateButton.setTitle("Display QR code", for: .normal)
-        navigateButton.titleLabel?.font = UIFont
-            .preferredFont(forTextStyle: .headline)
-        navigateButton.translatesAutoresizingMaskIntoConstraints = false
-        navigateButton
-            .addTarget(
-                self,
-                action: #selector(navigateButtonTapped),
-                for: .touchUpInside
-            )
-
-        view.addSubview(navigateButton)
-
         NSLayoutConstraint.activate([
-            navigateButton.centerXAnchor
-                .constraint(equalTo: view.centerXAnchor),
-            navigateButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            presentButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            presentButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
-    }
-    
-    private func setupActivityIndicator() {
+
         activityIndicator.translatesAutoresizingMaskIntoConstraints = false
         activityIndicator.hidesWhenStopped = true
+        activityIndicator.accessibilityIdentifier = HolderViewController.activityIndicatorIdentifier
         view.addSubview(activityIndicator)
 
         NSLayoutConstraint.activate([
             activityIndicator.centerXAnchor
                 .constraint(equalTo: view.centerXAnchor),
             activityIndicator.centerYAnchor
-                .constraint(equalTo: navigateButton.topAnchor, constant: -62)
+                .constraint(equalTo: presentButton.topAnchor, constant: -62)
         ])
     }
 
-    @objc private func navigateButtonTapped() {
+    @objc private func presentButtonTapped() {
         activityIndicator.startAnimating()
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
             self.navigateToQRCodeView()
