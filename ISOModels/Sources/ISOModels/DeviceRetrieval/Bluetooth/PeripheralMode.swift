3775dde765f4d453c2a013459cbf32583ab03c38
jwinterschladen-dd <jack.winterschladen@digital.cabinet-office.gov.uk>
2026-01-13 11:45:45 +0000

    Remove mock address param

--- ISOModels/Sources/ISOModels/DeviceRetrieval/Bluetooth/PeripheralMode.swift
+++ ISOModels/Sources/ISOModels/DeviceRetrieval/Bluetooth/PeripheralMode.swift
@@ -5,7 +5,7 @@ public struct PeripheralMode {
     public let uuid: UUID
     public let address: String?
     
-    public init(uuid: UUID, address: String?) {
+    public init(uuid: UUID, address: String? = nil) {
         self.uuid = uuid
         self.address = address
     }
