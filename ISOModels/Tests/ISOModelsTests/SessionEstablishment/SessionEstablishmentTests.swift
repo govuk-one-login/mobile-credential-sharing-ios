4ac3c1b09d81d511c21b643217c71fc7d8981611
jwinterschladen-dd <jack.winterschladen@digital.cabinet-office.gov.uk>
2026-01-19 18:08:27 +0000

    Add tests for CredentialPresenter & SeshEstablis

--- ISOModels/Tests/ISOModelsTests/SessionEstablishment/SessionEstablishmentTests.swift
+++ ISOModels/Tests/ISOModelsTests/SessionEstablishment/SessionEstablishmentTests.swift
@@ -89,6 +89,46 @@ struct SessionEstablishmentTests {
             }
         }
     }
+    
+    @Test("Invalid data structure - no CBOR 'eReaderKey' - throws an error on decoding")
+    func invalidCBORNoEReaderKeyThrowsAnError() throws {
+        let noEReaderKeyField = try #require(Data(base64Encoded: "oWRkYXRhWQLfUq2irL62w5DyygvGWbSEZ465TdRQdDhqreziN3e0RgbkLihGvC4u48HoZ7HRaF5BNUoCGrsP2jbwnPXVxRtWHTvkHJNHrnHPK0nenex7RARqsCJHkxshDJFXhAwVFKYCewiBBxat9hlmNEl5MUrDrp9A5m4BXBJUpoQQi9CT6HcuwzP7Zj/WgDrwLqEL2+g6mZ91tVoYD4chOftXrASs1YyhXsoVDN4cO4SUARiLejDOiH3XtxsS7aL8bsblI1pslJg1H80wHyKSpOu6dVUoXO6E6tlu8Wd7Cvgjn2p6Uq9LiAmx1SqyGhYsoxreIcV70dmXCigyqsQcfVLRxP7k7mQDCiGN9RNjvnAXkvpsUVxIm9Odytb7pI8dbrGenHaVMaO/mZijLAGEEwXyOETKPbah/w0NkXND1i/HKtWOqwGjGYEW8ZYGYJ+U416st40jxZxnhSo2GRX+h4SM26VjDJn6txrv9y0THPRCZU93COxIIWQW8tmWz2z5EBK3cbiJB7HRYp36eUND5lPDEgdILi9mIc1LXc87PDKGJcM/6YvpnF8mSiZDFb5Buv3HJvi83lkg3gpxiE2GCvRMH/Gz14sujXINhdrlP+orP6GAYWKkvgLQOVZ8XrJBnCrYea9I/LffVcqU8bAPYhh/ojKcgieq4BMOwFLKPiEC5X5ykRsyjP3Puq9rk2RmD2E0FTgmRMMMC9TiIsXPlLpac2ecU9XO2VylB4fCKJoMFzWDk8Hg8icjYQAvubFgYGiIpZ73osOJ9ot8tCRXLbAmsXzyvcr8tnyCktkrUAUDVpAKYqgrFvhUdZBSsA8PRnOkYin0Mlfo6DJUAbP+zIxtIli69/fC+7r6s6G2re1Ozqwer9W2ERjfk7wKYisDUE/eR867Ik6YPbEmd+MWwiquBC1s5K2uDYsPQEN7jhr6CFnJUBvrY5dEloWaYPEQabGWW0/6xXealhkfierHyqaIueZ8"))
+        
+        #expect(
+            throws: SessionEstablishmentError.cborEReaderKeyFieldMissing
+        ) {
+            try SessionEstablishment(data: noEReaderKeyField)
+        }
+    }
+    
+    @Test("Invalid data structure - no CBOR 'data' - throws an error on decoding")
+    func invalidCBORNoDataThrowsAnError() throws {
+        let noDataField = try #require(Data(base64Encoded: "oWplUmVhZGVyS2V52BhYS6QBAiABIVggYOM5I4UEH1FAMFHyQVUxy1bdP5mccWhwE6rGdovIGH4iWCDljeuP2+kH991TaCRVUaNHlvfSIVxEDDObsPe2e+zN+g=="))
+        
+        #expect(
+            throws: SessionEstablishmentError.cborDataFieldMissing
+        ) {
+            try SessionEstablishment(data: noDataField)
+        }
+    }
+    
+    @Test("SessionEstablishmentError descriptions are correct")
+    func peripheralErrorDescriptions() {
+        for error in [
+            SessionEstablishmentError.cborMapMissing,
+            .cborEReaderKeyFieldMissing,
+            .cborDataFieldMissing
+        ] {
+            switch error {
+            case .cborMapMissing:
+                #expect(error.errorDescription == "CBOR decoding error: SessionEstablishment contains invalid CBOR encoding (status code 11 CBOR decoding error)")
+            case .cborEReaderKeyFieldMissing:
+                #expect(error.errorDescription == "CBOR parsing error: SessionEstablishment missing mandatory key 'eReaderKey' (status code 12 CBOR validation error)")
+            case .cborDataFieldMissing:
+                #expect(error.errorDescription == "CBOR parsing error: SessionEstablishment missing mandatory key 'data' (status code 12 CBOR validation error)")
+            }
+        }
+    }
 }
 
 // swiftlint:enable line_length
