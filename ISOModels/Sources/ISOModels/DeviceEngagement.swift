01431e7b554966cc922c9ca21c48d3507d041aeb
Mike Linford <mlinford@deloitte.co.uk>
2026-01-23 17:08:28 +0000

    can now decode the qr into a cbor map - have extracted the version and security from the map.

--- ISOModels/Sources/ISOModels/DeviceEngagement.swift
+++ ISOModels/Sources/ISOModels/DeviceEngagement.swift
@@ -4,6 +4,25 @@ import SwiftCBOR
 enum DeviceEngagementError: Error {
     case requestWasIncorrectlyStructured
     case unsupportedRequest
+    case missingVersion
+    case missingSecurity
+    case missingRetrievalMethods
+    
+    public var errorDescription: String? {
+        switch self {
+        case .requestWasIncorrectlyStructured:
+            return "The request was incorrectly structured"
+        case .unsupportedRequest:
+            return "That request is not supported"
+        case .missingVersion:
+            return "The version number is missing"
+        case .missingSecurity:
+            return "The security is missing from the map"
+        case .missingRetrievalMethods:
+            return "The retrieval methods are missing from the map"
+        }
+
+    }
 }
 
 public struct DeviceEngagement {
@@ -36,7 +55,44 @@ extension DeviceEngagement: CBOREncodable {
         .security: security.toCBOR(options: options),
         .deviceRetrievalMethods: deviceRetrievalMethods!.toCBOR()
     ])
-}
+    }
+    
+    public static func decode() throws -> [CBOR: CBOR] {
+        let exampleString: String = "owBjMS4wAYIB2BhYS6QBAiABIVggVfvhhCVTTs1tL-6aQemxecCx_E1iL-F8vnKhlli9aAUiWCB_Dv4CTLvQ3ywTKQuEoDSZ9wnDq5aFJGLfJFNAsOqy5QKBgwIBowD1AfQKUGyqBZ4EGkU_kCmGmL9VmAk"
+        
+        guard let exampleData: Data = Data(base64URLEncoded: exampleString) else {
+            throw DeviceEngagementError.requestWasIncorrectlyStructured
+        }
+        guard let exampleCBOR: CBOR = try CBOR.decode([UInt8](exampleData)) else {
+            throw DeviceEngagementError.requestWasIncorrectlyStructured
+        }
+        guard case .map(let exampleCBORMap) = exampleCBOR else {
+            throw DeviceEngagementError.requestWasIncorrectlyStructured
+        }
+        
+        guard case .utf8String(let version) = exampleCBOR[.version] else {
+            throw DeviceEngagementError.missingVersion
+        }
+        
+        guard case .array(let securityArray) = exampleCBOR[.security] else {
+            throw DeviceEngagementError.missingSecurity
+        }
+
+        guard case .unsignedInt(let security) = securityArray[0] else {
+            throw DeviceEngagementError.missingSecurity
+        }
+        
+        guard case .array(let retrievalArray) = exampleCBOR[.deviceRetrievalMethods] else {
+            throw DeviceEngagementError.missingRetrievalMethods
+        }
+
+        print("Retrieval Array: \(retrievalArray)")
+        print("Security: \(security)")
+        print("Version: \(version)")
+        return exampleCBORMap
+        
+        
+    }
 }
 
 fileprivate extension CBOR {
