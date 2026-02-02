55cc0370bb91b2e68183dd087d641cf61c02c797
Richard Das <richard.das@digital.cabinet-office.gov.uk>
2026-02-02 15:13:15 +0000

    Updated print statement to match AC

--- Bluetooth/Sources/Bluetooth/PeripheralSession.swift
+++ Bluetooth/Sources/Bluetooth/PeripheralSession.swift
@@ -78,7 +78,7 @@ extension PeripheralSession {
                 for: stateChar,
                 onSubscribedCentrals: nil
             )
-            print("GATT Notified 'State' characteristic with: \(sent)")
+            print("GATT Notified 'State' characteristic with: \(ConnectionState.end.data)")
             if !sent {
                 print("Failed to notify GATT end command")
                 onError(.failedToNotifyEnd)
