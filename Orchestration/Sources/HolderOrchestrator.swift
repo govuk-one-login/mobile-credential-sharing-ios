08edc9131dd0d04827cb0756844193f05c864bd7
jwinterschladen-dd <jack.winterschladen@digital.cabinet-office.gov.uk>
2026-02-09 13:59:10 +0000

    Add delegate method to orchestrator for updating BT state

--- Orchestration/Sources/HolderOrchestrator.swift
+++ Orchestration/Sources/HolderOrchestrator.swift
@@ -1,4 +1,5 @@
 import Foundation
+import CoreBluetooth
 import PrerequisiteGate
 
 public protocol HolderOrchestratorProtocol {
@@ -6,8 +7,9 @@ public protocol HolderOrchestratorProtocol {
     func cancelPresentation()
 }
 
-public class HolderOrchestrator: HolderOrchestratorProtocol {
+public class HolderOrchestrator: HolderOrchestratorProtocol, PrerequisiteGateDelegate {
     private(set) var session: HolderSession?
+    var waiting: Bool = false
     
     // We must maintain a strong reference to PrerequisiteGate to enable the CoreBluetooth OS prompt to be displayed
     private(set) var prerequisiteGate: PrerequisiteGate?
@@ -21,32 +23,59 @@ public class HolderOrchestrator: HolderOrchestratorProtocol {
         print("Holder Presentation Session started")
         
         // MARK: - PrerequisiteGate
-        var permissionsToRequest = PrerequisiteGate.checkCapabilities(for: [.bluetooth])
+        beginPrerequisiteFlow()
+            
+            
+        // At this point we must wait for the TemporaryPeripheralManagerDelegate.peripheralManagerDidUpdateState() function to detect that the permissions have been updated
+            
+            
+        // TODO: Once we've determined the permissions have been accpeted from the delegate method, we can checkCapabilities again and resume the journey.
+        
+     
+    }
+
+    func beginPrerequisiteFlow() {
+        var permissionsToRequest = PrerequisiteGate.checkCapabilities(
+            for: [.bluetooth]
+        )
         do {
             try session?.transition(
                 to: .preflight(missingPermissions: permissionsToRequest)
             )
             
             // TODO: Request permissions on UI
-//            delegate.requestPermissions(for: permissionsToRequest)
+            //            delegate.render(for: session.currentState)
+            
             // Temporary request before UI impl
             requestPermission(for: .bluetooth)
-            
-            // At this point we must wait for the TemporaryPeripheralManagerDelegate.peripheralManagerDidUpdateState() function to detect that the permissions have been updated
-            
-            
-            // TODO: Once we've determined the permissions have been accpeted from the delegate method, we can checkCapabilities again and resume the journey.
-            permissionsToRequest = PrerequisiteGate.checkCapabilities(for: [.bluetooth])
-            
-            guard permissionsToRequest.isEmpty else {
-                // throw error
+        } catch {
+        }
+    }
+
+    
+    public func didUpdatePermissions() {
+        let permissionsToRequest = PrerequisiteGate.checkCapabilities(
+            for: [.bluetooth]
+        )
+
+        if permissionsToRequest.isEmpty {
+            do {
+                try session?.transition(to: .readyToPresent)
+                print(session?.currentState)
+            } catch  {
+                
+            }
+            // doNextFunc()
+        } else {
+            guard CBManager.authorization != .denied else {
+//            TODO: Render error screen if BLE permission is denied
+//                delegate.render(for: session.currentState)
+                print("Permissions denied, show UI to request permissions")
                 return
             }
-            try session?.transition(to: .readyToPresent)
-            print(session?.currentState)
-        } catch {
-            
+            beginPrerequisiteFlow()
         }
+        
     }
     
     public func cancelPresentation() {
@@ -57,6 +86,8 @@ public class HolderOrchestrator: HolderOrchestratorProtocol {
     // TODO: To be called from UI layer
     public func requestPermission(for capability: Capability) {
         prerequisiteGate = PrerequisiteGate()
+        prerequisiteGate?.delegate = self
         prerequisiteGate?.requestPermission(for: capability)
     }
 }
+
