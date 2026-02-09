218a20af963a34f67f3febcbcb00decc1857caf9
jwinterschladen-dd <jack.winterschladen@digital.cabinet-office.gov.uk>
2026-02-09 17:14:57 +0000

    Add PrerequisiteGate tests

---
+++ PrerequisiteGate/Sources/Capability.swift
@@ -0,0 +1,19 @@
+import AVFoundation
+import CoreBluetooth
+
+public enum Capability: CaseIterable, Sendable {
+    case bluetooth
+    case camera
+   
+    var isAllowed: Bool {
+        switch self {
+        case .bluetooth:
+            return CBManager.authorization == .allowedAlways
+            //            &&
+            //            CBManager.state == .poweredOn
+        case .camera:
+            return AVCaptureDevice.default(for: .video) != nil &&
+            AVCaptureDevice.authorizationStatus(for: .video) == .authorized
+        }
+    }
+}
