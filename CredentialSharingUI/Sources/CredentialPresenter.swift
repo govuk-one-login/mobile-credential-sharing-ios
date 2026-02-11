be3b70969f93546179875d30cc7f16ec78f1e299
Tyler Morrison <tmmorrison@deloitte.co.uk>
2026-02-11 13:03:25 +0000

    feat: [DCMAW-17062] (WIP) initial implementation + Todo's outlined

--- CredentialSharingUI/Sources/CredentialPresenter.swift
+++ CredentialSharingUI/Sources/CredentialPresenter.swift
@@ -197,7 +197,9 @@ private struct CryptoService {
             by: .reader
         )
         print("session establisment data: \(sessionEstablishment.data)")
-        
+
+        // TODO: Keep if SKReaderKey will be in CBOR or not (Richard to confirm)
+        /*
         guard let cborData = try CBOR.decode(sessionEstablishment.data) else {
             print("oops")
             return
@@ -209,26 +211,73 @@ private struct CryptoService {
             print("can't convert cbor to bytestring")
             return
         }
-        
-        let data = Data([0x00, 0x00, 0x00, 0x01])
+        */
+        var messageCounter = 1
+        let iv = constructIV(
+            messageCounter: messageCounter
+        )
 
-        let value = data.withUnsafeBytes {
-            $0.load(as: UInt32.self)
-        }.bigEndian
+        // Increment message Counter after successful decryption
+        messageCounter += 1
 
-        print(value)
-        print("encryptedData: \(encryptedData)")
-        
-        let iv = try AES.GCM.Nonce(data: encryptedData.prefix(12))
-        
-        let cipherText = encryptedData.suffix(from: 12)
-        
-        let sealedBox = try AES.GCM.SealedBox(nonce: iv, ciphertext: cipherText, tag: Data())
-        
-//        let decryptedData = try AES.GCM.open(sealedBox, using: sessionEstablishment.eReaderKey)
-        // TODO: DCMAW-17062 - Further decryption of data to be done here
+        print("IV: \(iv)")
+
+        let temp = try AES.GCM.Nonce(data: iv)
+        print("temp: \(temp)")
+
+        let cipherText = sessionEstablishment.data.dropLast(16) // Assuming the last 16 bytes are the tag
+        let authenticationTag = sessionEstablishment.data.suffix(16)
+
+        // TODO: Remove these 3 once we get Richard's SKReaderKey and can properly construct the SymmetricKey for decryption
+        let skreader = Data("58d277d8719e62a1561d248f403f477e9e6c37bf5d5fc5126f8f4c727c22dfc9".utf8)
+        let skreaderUInt8 = [UInt8](skreader)
+        print("skreaderUInt8 count: \(skreaderUInt8.count)")
+
+        let skreader2 = Data(base64Encoded: "58d277d8719e62a1561d248f403f477e9e6c37bf5d5fc5126f8f4c727c22dfc9")
+        let skreader2UInt8 = [UInt8](skreader2 ?? Data())
+        print("skreader2UInt8 count: \(skreader2UInt8.count)")
+
+        let skreader3 = Data(hex: "58d277d8719e62a1561d248f403f477e9e6c37bf5d5fc5126f8f4c727c22dfc9")
+        print("skreader3 count: \(skreader3?.count ?? 0)")
+
+
+
+        let symmetricKey = SymmetricKey(data: skreader3 ?? Data())
+
+        let sealedBox = try AES.GCM.SealedBox(
+            nonce: temp,
+            ciphertext: cipherText,
+            tag: authenticationTag
+        )
+
+        print("sealedBox: \(sealedBox)")
+        var decryptedData: Data = Data()
+        do {
+            decryptedData = try AES.GCM.open(
+                sealedBox,
+                using: symmetricKey
+            )
+        } catch {
+            print("Decryption failed: \(error)")
+        }
+
+        print("decryptedData: \(decryptedData)")
+        let decryptedString = String(data: decryptedData, encoding: .utf8)
+        //print("decryptedString: \(decryptedString)")
+        // TODO: 1.) Get SKReaderKey from Richard's ticket, 2.) Testing once functional, 3.) Authentication Tag (last 12 bytes) validation, 4.) Cleanup
     }
-    
+
+    private func constructIV(messageCounter: Int) -> Data {
+        let identifier: [UInt8] = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+        let convertedMessageCounter: Int32 = Int32(messageCounter)
+        let messageCounterArray = withUnsafeBytes(of: convertedMessageCounter.bigEndian, Array.init)
+        print(messageCounterArray)
+        print("identifier: \(identifier)")
+        let iv = identifier + messageCounterArray
+        return Data(iv)
+    }
+
+
     private func createSessionTranscriptBytes(with deviceEngagementBytes: [UInt8], and eReaderKeyBytes: [UInt8]) -> [UInt8] {
         let sessionTranscript = SessionTranscript(
             deviceEngagementBytes: deviceEngagementBytes,
@@ -241,5 +290,25 @@ private struct CryptoService {
             .toCBOR(options: CBOROptions())
             .asDataItem(options: CBOROptions())
             .encode()
+    }
+}
+// TODO: Remove this temp 32 byte init
+extension Data {
+    init?(hex: String) {
+        let hex = hex.count % 2 == 0 ? hex : "0" + hex
+        var data = Data(capacity: hex.count / 2)
+
+        var index = hex.startIndex
+        while index < hex.endIndex {
+            let nextIndex = hex.index(index, offsetBy: 2)
+            guard nextIndex <= hex.endIndex,
+                  let byte = UInt8(hex[index..<nextIndex], radix: 16) else {
+                return nil
+            }
+            data.append(byte)
+            index = nextIndex
+        }
+
+        self = data
     }
 }
