4bca4e1230f0a3b9a6c24981348cde8be6b87d0c
Tyler Morrison <tmmorrison@deloitte.co.uk>
2026-02-13 14:44:02 +0000

    feat: [DCMAW-17062] final PR Comment changes

--- CredentialSharingUI/Sources/CredentialPresenter.swift
+++ CredentialSharingUI/Sources/CredentialPresenter.swift
@@ -2,7 +2,6 @@ import BluetoothTransport
 import CoreBluetooth
 import CryptoKit
 import CryptoService
-internal import SwiftCBOR
 import UIKit
 
 @MainActor
@@ -167,55 +166,5 @@ extension CredentialPresenter: @MainActor QRCodeViewControllerDelegate {
         qrCodeViewController?.dismiss(animated: false)
         let errorViewController = ErrorViewController(titleText: titleText)
         navigationController?.pushViewController(errorViewController, animated: true)
-    }
-}
-
-private struct CryptoService {
-    var sessionDecryption: SessionDecryption
-    var messageCounter: Int = 1 // Will likely need to move to HolderSession once it is implemented here
-
-    mutating func decryptSessionEstablishmentMessage(from messageData: Data, with deviceEngagement: DeviceEngagement) throws {
-        // Decode the SessionEstablishment message
-        let sessionEstablishment = try SessionEstablishment(
-            rawData: messageData
-        )
-        
-        // Generate the PublicKey using the EReaderKey (COSEKey)
-        let eReaderKey = try P256.KeyAgreement.PublicKey(
-            coseKey: sessionEstablishment.eReaderKey
-        )
-
-        print("eReaderKey: \(eReaderKey)")
-
-        // Generate the SessionTranscriptBytes
-        let sessionTranscriptBytes = createSessionTranscriptBytes(with: deviceEngagement.encode(options: CBOROptions()), and: sessionEstablishment.eReaderKeyBytes)
-        print("sessionEstablishment.data: \(sessionEstablishment.data)")
-        // Decrypt the data
-        do {
-            let decryptedData = try sessionDecryption.decryptData(
-                sessionEstablishment.data,
-                salt: sessionTranscriptBytes,
-                encryptedWith: eReaderKey,
-                by: .reader
-            )
-            messageCounter = 2
-            print("decryptedData: \(decryptedData.base64EncodedString())")
-        } catch {
-            messageCounter = 1
-        }
-    }
-
-    private func createSessionTranscriptBytes(with deviceEngagementBytes: [UInt8], and eReaderKeyBytes: [UInt8]) -> [UInt8] {
-        let sessionTranscript = SessionTranscript(
-            deviceEngagementBytes: deviceEngagementBytes,
-            eReaderKeyBytes: eReaderKeyBytes,
-            handover: .qr
-        )
-        print("SessionTranscript constructed successfully: \(sessionTranscript)")
-        
-        return sessionTranscript
-            .toCBOR(options: CBOROptions())
-            .asDataItem(options: CBOROptions())
-            .encode()
     }
 }
