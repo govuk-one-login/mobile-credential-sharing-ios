80fb89580016b27a7de647fd3c27d7c734c6b284
jwinterschladen-dd <jack.winterschladen@digital.cabinet-office.gov.uk>
2026-02-19 10:43:45 +0000

    Remove aDecoder surplus param

--- CredentialSharingUI/Sources/HolderContainer.swift
+++ CredentialSharingUI/Sources/HolderContainer.swift
@@ -11,7 +11,7 @@ public class HolderContainerNavigation: UINavigationController {
         self.init(holderContainer: HolderContainer())
     }
     
-    required init?(coder aDecoder: NSCoder) {
+    required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
