9626530913480194f28027c4edad075dfd4a0094
jwinterschladen-dd <jack.winterschladen@digital.cabinet-office.gov.uk>
2026-02-20 13:31:25 +0000

    Fix linting

--- PrerequisiteGate/Tests/Mocks/MockCBPeripheralManager.swift
+++ PrerequisiteGate/Tests/Mocks/MockCBPeripheralManager.swift
@@ -3,7 +3,7 @@ import CoreBluetooth
 
 class MockCBPeripheralManager: PeripheralManager {
     nonisolated(unsafe) static var initCalled = false
-    nonisolated(unsafe) static var options: [String: Any]? = nil
+    nonisolated(unsafe) static var options: [String: Any]?
     required init(
         delegate: (any CBPeripheralManagerDelegate)?,
         queue: dispatch_queue_t?,
