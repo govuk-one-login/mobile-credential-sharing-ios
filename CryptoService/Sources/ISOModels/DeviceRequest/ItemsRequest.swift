6ab2e162c7a678c62ddcb015fb7623ffd7245e91
jwinterschladen-dd <jack.winterschladen@digital.cabinet-office.gov.uk>
2026-03-09 11:52:43 +0000

    Change DocumentType to DocType to match spec

--- CryptoService/Sources/ISOModels/DeviceRequest/ItemsRequest.swift
+++ CryptoService/Sources/ISOModels/DeviceRequest/ItemsRequest.swift
@@ -1,21 +1,21 @@
 import SwiftCBOR
 
 struct ItemsRequest {
-    let documentType: DocumentType
+    let docType: DocType
     let nameSpaces: [NameSpace]
     
     init(cbor: CBOR) throws {
         guard case .map(let request) = cbor,
-              case .utf8String(let docType) = request[.docType],
+              case .utf8String(let rawDocType) = request[.docType],
               case .map(let nameSpaces) = request[.nameSpaces]
         else {
             throw DeviceRequestError.itemsRequestWasIncorrectlyStructured
         }
         
-        guard let documentType = DocumentType(rawValue: docType) else {
+        guard let docType = DocType(rawValue: rawDocType) else {
             throw DeviceRequestError.unsupportedDocumentType
         }
-        self.documentType = documentType
+        self.docType = docType
         self.nameSpaces = try nameSpaces.map {
             guard case .utf8String(let name) = $0 else {
                 throw DeviceRequestError.itemsRequestWasIncorrectlyStructured
--- CryptoService/Sources/ISOModels/DocType.swift
+++ CryptoService/Sources/ISOModels/DocType.swift
@@ -1,3 +1,3 @@
-public enum DocumentType: String {
+public enum DocType: String {
     case mdl = "org.iso.18013.5.1.mDL"
 }
