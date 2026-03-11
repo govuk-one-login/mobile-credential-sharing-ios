26d07ffb3f32d3852056d25fb942dc6d6b551861
jwinterschladen-dd <jack.winterschladen@digital.cabinet-office.gov.uk>
2026-03-11 15:41:48 +0000

    Remove comment from mock

--- CryptoService/Tests/Mocks/MockSessionDecryption.swift
+++ CryptoService/Tests/Mocks/MockSessionDecryption.swift
@@ -6,9 +6,12 @@ class MockSessionDecryption: Decryption {
     var publicKey: P256.KeyAgreement.PublicKey = P256.KeyAgreement.PrivateKey().publicKey
     var decryptedDataToReturn = Data()
     
-    func decryptData(_ data: [UInt8], salt: [UInt8], encryptedWith theirPublicKey: P256.KeyAgreement.PublicKey, by parameters: any EncryptionParameters) throws -> Data {
-        // So long as this function doesn't throw, it will be treated as a success
-        
+    func decryptData(
+        _ data: [UInt8],
+        salt: [UInt8],
+        encryptedWith theirPublicKey: P256.KeyAgreement.PublicKey,
+        by parameters: any EncryptionParameters
+    ) throws -> Data {
         return decryptedDataToReturn
     }
 }
