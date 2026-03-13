af8875ebf7917c53e3d73b8ae171c4671079b36d
jwinterschladen-dd <jack.winterschladen@digital.cabinet-office.gov.uk>
2026-03-13 12:46:33 +0000

    Extract DeviceRequestError to own file

--- SharingCryptoService/Sources/ISOModels/DeviceRequest/DeviceRequest.swift
+++ SharingCryptoService/Sources/ISOModels/DeviceRequest/DeviceRequest.swift
@@ -1,34 +1,6 @@
 import Foundation
 import SwiftCBOR
 
-enum DeviceRequestError: LocalizedError {
-    case dataIsNotValidCBOR
-    case deviceRequestWasIncorrectlyStructured
-    case docRequestWasEmpty
-    case docRequestWasIncorrectlyStructured
-    case itemsRequestWasIncorrectlyStructured
-    case nameSpaceWasIncorrectlyStructured
-    case unsupportedDocumentType
-    
-    var errorDescription: String? {
-        return "\(self): status code \(self.statusCode)"
-    }
-    
-    var statusCode: Int {
-        switch self {
-        case .dataIsNotValidCBOR:
-            return 11
-        case .deviceRequestWasIncorrectlyStructured,
-                .docRequestWasEmpty,
-                .docRequestWasIncorrectlyStructured,
-                .itemsRequestWasIncorrectlyStructured,
-                .nameSpaceWasIncorrectlyStructured,
-                .unsupportedDocumentType:
-            return 20
-        }
-    }
-}
-
 public struct DeviceRequest {
     let version: String
     let docRequests: [DocRequest]
