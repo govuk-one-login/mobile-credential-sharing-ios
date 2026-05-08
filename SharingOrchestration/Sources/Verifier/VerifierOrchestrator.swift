662ed9d0ffdf80c64e4570991da04e14bd8ffce5
jwinterschladen-dd <jack.winterschladen@digital.cabinet-office.gov.uk>
2026-05-08 09:56:17 +0100

    Add initial VerifierOrchestrator

---
+++ SharingOrchestration/Sources/Verifier/VerifierOrchestrator.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+public protocol VerifierOrchestratorProtocol {
+    var delegate: VerifierOrchestratorDelegate? { get set }
+    func startVerification()
+    func cancelVerification()
+}
+
+public protocol VerifierOrchestratorDelegate: AnyObject {
+    func verifierOrchestrator(didStart: Bool)
+    func verifierOrchestrator(didCancel: Bool)
+}
+
+public class VerifierOrchestrator: VerifierOrchestratorProtocol {
+    public weak var delegate: VerifierOrchestratorDelegate?
+    private(set) public var isActive: Bool = false
+
+    public init() {}
+
+    public func startVerification() {
+        isActive = true
+        print("Verifier journey started")
+        delegate?.verifierOrchestrator(didStart: true)
+    }
+
+    public func cancelVerification() {
+        isActive = false
+        print("Verifier journey cancelled")
+        delegate?.verifierOrchestrator(didCancel: true)
+    }
+}
