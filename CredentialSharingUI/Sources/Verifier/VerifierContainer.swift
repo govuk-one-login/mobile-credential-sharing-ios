f13a2c02317a0f6eb9cfe779c8b43bedeb8f26ea
jwinterschladen-dd <jack.winterschladen@digital.cabinet-office.gov.uk>
2026-05-08 13:26:43 +0100

    Add VerifierContainer

---
+++ CredentialSharingUI/Sources/Verifier/VerifierContainer.swift
@@ -0,0 +1,32 @@
+import SharingOrchestration
+import UIKit
+
+@MainActor
+class VerifierContainer: UIViewController {
+    var orchestrator: VerifierOrchestratorProtocol
+
+    init(orchestrator: VerifierOrchestratorProtocol = VerifierOrchestrator()) {
+        self.orchestrator = orchestrator
+        super.init(nibName: nil, bundle: nil)
+        self.orchestrator.delegate = self
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        orchestrator.startVerification()
+    }
+}
+
+extension VerifierContainer: @MainActor VerifierOrchestratorDelegate {
+    func verifierOrchestrator(didStart: Bool) {
+        // Future: navigate to scanning UI
+    }
+
+    func verifierOrchestrator(didCancel: Bool) {
+        navigationController?.dismiss(animated: true)
+    }
+}
