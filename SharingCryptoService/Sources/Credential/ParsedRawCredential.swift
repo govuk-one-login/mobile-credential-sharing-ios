29ed58ae9c2ccabcc4427f905f46ca9c5e576f60
jwinterschladen-dd <jack.winterschladen@digital.cabinet-office.gov.uk>
2026-05-13 15:39:58 +0100

    Add nameSpaces & issuerAuth to ParsedRawCred

--- SharingCryptoService/Sources/Credential/ParsedRawCredential.swift
+++ SharingCryptoService/Sources/Credential/ParsedRawCredential.swift
@@ -1,5 +1,21 @@
 import Foundation
+import SwiftCBOR
 
 public struct ParsedRawCredential {
-    public var docType: String
+    public let docType: String
+    public let nameSpaces: [String: [IssuerSignedItemBytes]]
+    public let issuerAuth: [UInt8]
+}
+
+/// Wraps a decoded elementIdentifier with the original Tag 24 CBOR for MSO integrity.
+public struct IssuerSignedItemBytes: Equatable, Sendable {
+    public let elementIdentifier: String
+    public let elementValue: CBOR
+    public let rawCBOR: CBOR
+
+    public init(elementIdentifier: String, elementValue: CBOR, rawCBOR: CBOR) {
+        self.elementIdentifier = elementIdentifier
+        self.elementValue = elementValue
+        self.rawCBOR = rawCBOR
+    }
 }
