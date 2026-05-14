9af201f8261cf3bd6f8971adccb4d6314312d190
jwinterschladen-dd <jack.winterschladen@digital.cabinet-office.gov.uk>
2026-05-14 15:54:21 +0100

    Add parseNameSpace error throw tests

--- SharingCryptoService/Tests/RawCredentialParserTests.swift
+++ SharingCryptoService/Tests/RawCredentialParserTests.swift
@@ -6,15 +6,22 @@ import Testing
 @Suite("RawCredentialParser Tests")
 struct RawCredentialParserTests {
     let sut = RawCredentialParser()
+    
+    // MARK: - Helpers
+    /// Builds a valid root CBOR (passes MSO parsing) with a custom nameSpaces value
+    private func validRootWithNameSpaces(_ nameSpaces: CBOR) -> CBOR {
+        let mso: CBOR = .map([.utf8String("docType"): .utf8String("org.iso.18013.5.1.mDL")])
+        let payload: CBOR = .tagged(.encodedCBORDataItem, .byteString(mso.encode()))
+        let issuerAuth: CBOR = .array([.null, .null, .byteString(payload.encode())])
+        return .map([
+            .utf8String("issuerAuth"): issuerAuth,
+            .utf8String("nameSpaces"): nameSpaces
+        ])
+    }
 
     @Test("Successfully parses a valid raw credential and extracts docType")
     func parsesValidCredential() throws {
         // Given
-        let mso: CBOR = .map([.utf8String("docType"): .utf8String("org.iso.18013.5.1.mDL")])
-        let msoBytes = mso.encode()
-        let payload: CBOR = .tagged(.encodedCBORDataItem, .byteString(msoBytes))
-        let payloadBytes = payload.encode()
-        let issuerAuth: CBOR = .array([.null, .null, .byteString(payloadBytes)])
         let itemCBOR: CBOR = .map([
             .utf8String("elementIdentifier"): .utf8String("family_name"),
             .utf8String("elementValue"): .utf8String("Smith")
@@ -24,10 +31,7 @@ struct RawCredentialParserTests {
                 .tagged(.encodedCBORDataItem, .byteString(itemCBOR.encode()))
             ])
         ])
-        let root: CBOR = .map([
-            .utf8String("issuerAuth"): issuerAuth,
-            .utf8String("nameSpaces"): nameSpaces
-        ])
+        let root: CBOR = validRootWithNameSpaces(nameSpaces)
         let data = Data(root.encode())
         
         // When
@@ -90,6 +94,88 @@ struct RawCredentialParserTests {
 
         // Then
         #expect(throws: RawCredentialParseError.msoDecodingFailed) {
+            try sut.parse(rawCredential: data)
+        }
+    }
+
+    // MARK: - parseNameSpaces error paths
+
+    @Test("Throws nameSpacesDecodingFailed when nameSpaces key is missing")
+    func throwsWhenNameSpacesMissing() {
+        // Given
+        let data = Data(validRootWithNameSpaces(.null).encode())
+
+        // Then
+        #expect(throws: RawCredentialParseError.nameSpacesDecodingFailed) {
+            try sut.parse(rawCredential: data)
+        }
+    }
+
+    @Test("Throws nameSpacesDecodingFailed when nameSpaces value is not a map")
+    func throwsWhenNameSpacesNotMap() {
+        // Given
+        let data = Data(validRootWithNameSpaces(.array([.null])).encode())
+
+        // Then
+        #expect(throws: RawCredentialParseError.nameSpacesDecodingFailed) {
+            try sut.parse(rawCredential: data)
+        }
+    }
+
+    @Test("Throws nameSpacesDecodingFailed when namespace value is not an array")
+    func throwsWhenNameSpaceValueNotArray() {
+        // Given
+        let nameSpaces: CBOR = .map([.utf8String("org.iso.18013.5.1"): .utf8String("invalid")])
+        let data = Data(validRootWithNameSpaces(nameSpaces).encode())
+
+        // Then
+        #expect(throws: RawCredentialParseError.nameSpacesDecodingFailed) {
+            try sut.parse(rawCredential: data)
+        }
+    }
+
+    @Test("Throws nameSpacesDecodingFailed when namespace key is not a string")
+    func throwsWhenNameSpaceKeyNotString() {
+        // Given
+        let nameSpaces: CBOR = .map([.unsignedInt(1): .array([])])
+        let data = Data(validRootWithNameSpaces(nameSpaces).encode())
+
+        // Then
+        #expect(throws: RawCredentialParseError.nameSpacesDecodingFailed) {
+            try sut.parse(rawCredential: data)
+        }
+    }
+
+    @Test("Throws nameSpacesDecodingFailed when item is not a Tag 24")
+    func throwsWhenItemNotTagged() {
+        // Given
+        let nameSpaces: CBOR = .map([
+            .utf8String("org.iso.18013.5.1"): .array([.utf8String("not a tag")])
+        ])
+        let data = Data(validRootWithNameSpaces(nameSpaces).encode())
+
+        // Then
+        #expect(throws: RawCredentialParseError.nameSpacesDecodingFailed) {
+            try sut.parse(rawCredential: data)
+        }
+    }
+
+    @Test("Throws nameSpacesDecodingFailed when item has no elementIdentifier")
+    func throwsWhenItemMissingElementIdentifier() {
+        // Given
+        let itemWithoutIdentifier: CBOR = .map([
+            .utf8String("digestID"): .unsignedInt(0),
+            .utf8String("elementValue"): .utf8String("value")
+        ])
+        let nameSpaces: CBOR = .map([
+            .utf8String("org.iso.18013.5.1"): .array([
+                .tagged(.encodedCBORDataItem, .byteString(itemWithoutIdentifier.encode()))
+            ])
+        ])
+        let data = Data(validRootWithNameSpaces(nameSpaces).encode())
+
+        // Then
+        #expect(throws: RawCredentialParseError.nameSpacesDecodingFailed) {
             try sut.parse(rawCredential: data)
         }
     }
