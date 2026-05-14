f61e493654039b7ba54ce6a2421f51c6fe4dcc00
jwinterschladen-dd <jack.winterschladen@digital.cabinet-office.gov.uk>
2026-05-14 14:47:36 +0100

    Fix line-length linting

--- CredentialSharingUI/Sources/CredentialProvider.swift
+++ CredentialSharingUI/Sources/CredentialProvider.swift
@@ -1,2 +1,3 @@
-/// CredentialProvider, CredentialRequest & Credential have been moved to the SharingOrchestration module and are imported here to preserve a single-import location in a Host App using CredentialSharingUI.
+/// CredentialProvider, CredentialRequest & Credential have been moved to the SharingOrchestration module
+/// and are imported here to preserve a single-import location in a Host App using CredentialSharingUI.
 @_exported import SharingOrchestration
