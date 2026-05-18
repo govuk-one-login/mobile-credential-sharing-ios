61deaff6544dd07674538a7413b01bce57945a59
Ifrah Hussein <ifrah.hussein@digital.cabinet-office.gov.uk>
2026-05-18 08:41:28 +0100

    chore: groups ui files

--- SharingOrchestration/Tests/VerifierOrchestratorTests.swift
+++ SharingOrchestration/Tests/VerifierOrchestratorTests.swift
@@ -13,48 +13,80 @@ struct VerifierOrchestratorTests {
 
     @Test("startVerification creates a new VerifierSession")
     func startVerificationCreatesSession() {
+        // Given
+        let sut = VerifierOrchestrator()
         #expect(sut.session == nil)
+
+        // When
         sut.startVerification()
+
+        // Then
         #expect(sut.session != nil)
     }
 
     @Test("cancelVerification releases the session")
     func cancelVerificationReleasesSession() {
+        // Given
+        let sut = VerifierOrchestrator()
         sut.startVerification()
         #expect(sut.session != nil)
+
+        // When
         sut.cancelVerification()
+
+        // Then
         #expect(sut.session == nil)
     }
 
     @Test("cancelVerification notifies delegate with cancelled state")
     func cancelVerificationNotifiesDelegate() {
+        // Given
+        let sut = VerifierOrchestrator()
         let delegate = MockVerifierOrchestratorDelegate()
         sut.delegate = delegate
         sut.startVerification()
+
+        // When
         sut.cancelVerification()
+
+        // Then
         #expect(delegate.stateToRender == .cancelled)
     }
 
     @Test("startVerification after cancel creates a new session instance")
     func startAfterCancelCreatesNewSession() throws {
+        // Given
+        let sut = VerifierOrchestrator()
         sut.startVerification()
+
         let firstSession = try #require(sut.session as? VerifierSession)
+
+        // When
         sut.cancelVerification()
         sut.startVerification()
+
         let secondSession = try #require(sut.session as? VerifierSession)
+
+        // Then
         #expect(firstSession !== secondSession)
     }
 
     @Test("cancelVerification on already cancelled session still releases")
     func cancelOnAlreadyCancelledStillReleases() {
+        // Given
+        let sut = VerifierOrchestrator()
         sut.startVerification()
         sut.cancelVerification()
         #expect(sut.session == nil)
+
+        // When
         sut.cancelVerification()
+
+        // Then
         #expect(sut.session == nil)
     }
 
-    // MARK: - Preflight Tests (AC1–AC5)
+    // MARK: - Preflight Tests
 
     @Test("startVerification evaluates the bluetooth prerequisite through the gate")
     func startVerificationEvaluatesBluetoothPrerequisite() {
@@ -158,9 +190,14 @@ struct VerifierOrchestratorTests {
 
     @Test("cancelVerification releases prerequisiteGate")
     func cancelVerificationReleasesPrerequisiteGate() {
+        // Given
         sut.startVerification()
         #expect(sut.prerequisiteGate != nil)
+        
+        // When
         sut.cancelVerification()
+        
+        // Then
         #expect(sut.prerequisiteGate == nil)
     }
 }
