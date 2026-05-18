fe8b799c1068be92569497d9935519f6e6e09344
Ifrah Hussein <ifrah.hussein@digital.cabinet-office.gov.uk>
2026-05-18 14:39:24 +0100

    chore: adds comment on SessionError to conform to one protocol

--- SharingOrchestration/Sources/SessionError.swift
+++ SharingOrchestration/Sources/SessionError.swift
@@ -3,6 +3,7 @@ import SharingPrerequisiteGate
 
 public indirect enum SessionError: LocalizedError, Equatable, Hashable, Sendable {
     case unrecoverablePrerequisite(MissingPrerequisite)
+    // TODO: DCMAW-19716 Update to support both HolderSessionState and VerifierSessionState e.g. make the states conform to one protocol
     case incorrectSessionState(HolderSessionState)
     case unknown
     case generic(String)
