2221aa3a78881e8b68e8b8c44858caa89d637cdb
jwinterschladen-dd <jack.winterschladen@digital.cabinet-office.gov.uk>
2026-06-15 11:44:10 +0100

    Add Comments to Protocols

--- SharingBluetoothTransport/Sources/CBProtocols/BluetoothCentralProtocol.swift
+++ SharingBluetoothTransport/Sources/CBProtocols/BluetoothCentralProtocol.swift
@@ -2,7 +2,10 @@ import CoreBluetooth
 import Foundation
 
 public protocol BluetoothCentralProtocol {
+    /// The unique, persistent identifier associated with the peer.
     var identifier: UUID { get }
+    
+    /// The maximum amount of data, in bytes, that the central can receive in a single notification or indication.
     var maximumUpdateValueLength: Int { get }
 }
 
--- SharingBluetoothTransport/Sources/CBProtocols/BluetoothPeripheralProtocol.swift
+++ SharingBluetoothTransport/Sources/CBProtocols/BluetoothPeripheralProtocol.swift
@@ -2,18 +2,37 @@ import CoreBluetooth
 import Foundation
 
 public protocol BluetoothPeripheralProtocol {
+    /// The current connection state of the peripheral.
     var state: CBPeripheralState { get }
+    
+    /// The name of the peripheral.
     var name: String? { get }
+    
+    /// The unique, persistent identifier associated with the peer.
     var identifier: UUID { get }
+    
+    /// The delegate object that will receive peripheral events.
     var delegate: CBPeripheralDelegate? { get set }
+    
+    /// A list of a peripheral's discovered services.
     var services: [CBService]? { get }
+    
+    /// The maximum amount of data, in bytes, that can be sent to a characteristic in a single write type.
     func maximumWriteValueLength(for type: CBCharacteristicWriteType) -> Int
+    
+    /// Discovers the specified services of the peripheral.
     func discoverServices(_ serviceUUIDs: [CBUUID]?)
+    
+    /// Discovers the specified characteristics of a service.
     func discoverCharacteristics(
         _ characteristicUUIDs: [CBUUID]?,
         for service: CBService
     )
+    
+    /// Sets notifications or indications for the value of a specified characteristic.
     func setNotifyValue(_ enabled: Bool, for characteristic: CBCharacteristic)
+    
+    /// Writes the value of a characteristic.
     func writeValue(
         _ data: Data,
         for characteristic: CBCharacteristic,
