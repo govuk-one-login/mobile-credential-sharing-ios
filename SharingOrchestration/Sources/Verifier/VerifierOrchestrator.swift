d77347086ccdd532b957ead5fa7bf2d4af3bace7
jwinterschladen-dd <jack.winterschladen@digital.cabinet-office.gov.uk>
2026-08-13 09:28:35 +0100

    Include if statement in Task to reduce repeated code

--- SharingOrchestration/Sources/Verifier/VerifierOrchestrator.swift
+++ SharingOrchestration/Sources/Verifier/VerifierOrchestrator.swift
@@ -397,15 +397,12 @@ public class VerifierOrchestrator: VerifierOrchestratorProtocol {
     /// Waits `gattEndDelay` ms after send-completion, then sends GATT End and tears down the session.
     private func performDelayedGATTEndAndTeardown(terminalState: VerifierSessionState) {
         let delay = gattEndDelay
-        if delay == 0 {
-            bluetoothTransport?.sendGattEnd()
-            transitionToTerminalStateAndTeardown(terminalState: terminalState)
-        } else {
-            Task { @MainActor in
+        Task { @MainActor in
+            if delay > 0 {
                 try? await Task.sleep(for: .milliseconds(delay))
-                self.bluetoothTransport?.sendGattEnd()
-                self.transitionToTerminalStateAndTeardown(terminalState: terminalState)
             }
+            self.bluetoothTransport?.sendGattEnd()
+            self.transitionToTerminalStateAndTeardown(terminalState: terminalState)
         }
     }
     
