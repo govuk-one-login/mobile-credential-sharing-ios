33ed52e1594bb1317f58b74d9e603d716d51c306
Ifrah Hussein <ifrah.hussein@digital.cabinet-office.gov.uk>
2026-08-17 14:56:48 +0100

    feat: adds new MockCredential and the SignStrategy property

--- mobile-credential-sharing-ios/mobile-credential-sharing-ios/MockCredentials/MockCredential.swift
+++ mobile-credential-sharing-ios/mobile-credential-sharing-ios/MockCredentials/MockCredential.swift
@@ -5,4 +5,19 @@ struct MockCredential {
     let displayName: String
     let rawCredential: Data
     let privateKey: Data
+    let signingStrategy: SigningStrategy
+
+    init(
+        id: String,
+        displayName: String,
+        rawCredential: Data,
+        privateKey: Data,
+        signingStrategy: SigningStrategy = .success
+    ) {
+        self.id = id
+        self.displayName = displayName
+        self.rawCredential = rawCredential
+        self.privateKey = privateKey
+        self.signingStrategy = signingStrategy
+    }
 }
