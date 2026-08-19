558a4090a98fa401d95d86df0830b7383c97df50
Veghal, Param <param.veghal@digital.cabinet-office.gov.uk>
2026-08-19 21:16:32 +0100

    Fixed change with last commit

--- SharingOrchestration/Sources/Verifier/VerifierOrchestrator.swift
+++ SharingOrchestration/Sources/Verifier/VerifierOrchestrator.swift
@@ -472,7 +472,7 @@ public class VerifierOrchestrator: VerifierOrchestratorProtocol {
         inactivityTimer?.start()
     }
 
-    // Tears-down the session and returns user back to a reset state
+    // Tears-down the session and returns user back to a resetted state
     func handleInactivityTimeout() {
         guard let session,
               session.currentState == .connecting || session.currentState == .verifying
@@ -491,7 +491,6 @@ public class VerifierOrchestrator: VerifierOrchestratorProtocol {
         } catch {
             delegate?.orchestrator(didUpdateState: .failed(.generic(error.localizedDescription)))
         }
-        transitionToTerminalStateAndTeardown(terminalState: .cancelled)
     }
 }
 
