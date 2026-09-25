eb3bc4bd692b7c26cef6df48aecc0c41962c566b
Robin Phillips <robin.phillips1@digital.cabinet-office.gov.uk>
2026-09-25 23:31:11 +0100

    Depend on GDSLogging (renamed Logging) branch of mobile-ios-logging
    mobile-ios-logging renamed its 'Logging' target/product to 'GDSLogging' to
    avoid a module-name collision with Apple swift-log (pulled by jwt-kit 5).
    Point at that branch and update the SharingOrchestration product reference.
    Temporary branch pin for testing; revert to a version once upstream releases.

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/govuk-one-login/mobile-ios-logging",
-            from: "7.0.2"
+            branch: "fix/rp/rename-logging-target-gdslogging"
         ),
         // Upstream swift-certificates. RFC 5280 directoryName prefix matching for the C6
         // ReaderAuth NameConstraints check is implemented locally as `PrefixNameConstraintsPolicy`
@@ -152,7 +152,7 @@ let package = Package(
                 "SharingPrerequisiteGate",
                 "SharingCryptoService",
                 "SharingLogging",
-                .product(name: "Logging", package: "mobile-ios-logging")
+                .product(name: "GDSLogging", package: "mobile-ios-logging")
             ],
             path: "SharingOrchestration/Sources"
         ),
